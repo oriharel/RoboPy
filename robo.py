4b6da15a1686f442eef12391818682ceec8c388b
Ori Harel <ori.harel@gmail.com>
2019-03-07 23:46:56 +0200

    we have motor!!

--- robo.py
+++ robo.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 from flask import Flask
+from adafruit_motorkit import MotorKit
 import threading
-import signal
 import sys
 
+kit = MotorKit()
+
+speed = 0.4
+reveres_speed = -0.4
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
@@ -22,113 +26,25 @@ def set_interval(func, sec):
     return t
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-BLUE_LIGHT = 10
-RED_LIGHT = 9
-GREEN_LIGHT = 11
-MOTION = 22
-LEFT_LINE = 27
-RIGHT_LINE = 17
+def drive_forward():
+    kit.motor1.throttle = 1
+    kit.motor2.throttle = 1
 
-GPIO.setup(BLUE_LIGHT, GPIO.OUT)
-GPIO.setup(RED_LIGHT, GPIO.OUT)
-GPIO.setup(GREEN_LIGHT, GPIO.OUT)
-GPIO.setup(MOTION, GPIO.IN)
+def drive_backward():
+    kit.motor1.throttle = -1
+    kit.motor2.throttle = -1
 
+def turn_left():
+    kit.motor1.throttle = -1
+    kit.motor2.throttle = 1
 
-def blue_light_on():
-    GPIO.output(BLUE_LIGHT, GPIO.HIGH)
+def turn_right():
+    kit.motor1.throttle = 1
+    kit.motor2.throttle = -1
 
-
-def blue_light_off():
-    GPIO.output(BLUE_LIGHT, GPIO.LOW)
-
-
-def red_light_on():
-    GPIO.output(RED_LIGHT, GPIO.HIGH)
-
-
-def red_light_off():
-    GPIO.output(RED_LIGHT, GPIO.LOW)
-
-
-def green_light_on():
-    GPIO.output(GREEN_LIGHT, GPIO.HIGH)
-
-
-def green_light_off():
-    GPIO.output(GREEN_LIGHT, GPIO.LOW)
-
-
-def toggle_blue_light():
-    blue_light_off()
-    blue_light_on()
-    time.sleep(1)
-    blue_light_off()
-
-
-def all_lights_off():
-    blue_light_off()
-    red_light_off()
-    green_light_off()
-
-
-set_interval(toggle_blue_light, 3)
-
-GPIO.add_event_detect(MOTION, GPIO.BOTH, bouncetime=300)
-
-
-def motion_detected(channel):
-    if GPIO.input(channel):
-        print "Motion ON Detected!"
-        green_light_on()
-        time.sleep(5)
-        green_light_off()
-
-
-GPIO.add_event_callback(MOTION, motion_detected)
-
-# set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
-
-# set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-
-GPIO.output(GPIO_TRIGGER, False)
-
-
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
-
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-
-    # time difference between start and arrival
-    time_elapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (time_elapsed * 34300) / 2
-
-    print ("Measured Distance = %.1f cm" % distance)
-
-
-set_interval(distance, 1)
+def stop_driving():
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
 
 app = Flask(__name__)
 
@@ -136,6 +52,31 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/forward')
+def forward():
+    drive_forward()
+    return 'Hello forward'
+
+@app.route('/backward')
+def backward():
+    drive_backward()
+    return 'Hello backward'
+
+@app.route('/left')
+def left():
+    turn_left()
+    return 'Hello left'
+
+@app.route('/right')
+def right():
+    turn_right()
+    return 'Hello right'
+
+@app.route('/stop')
+def stop():
+    stop_driving()
+    return 'Hello stop'
 
 
 if __name__ == '__main__':
