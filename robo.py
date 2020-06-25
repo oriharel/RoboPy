8e0b4b1c4e3e23c51e92b9dadcabb6f846694be7
Ori Harel <ori@taranis.ag>
2020-06-25 23:22:36 +0300

    fdfg

--- robo.py
+++ robo.py
@@ -1,29 +1,41 @@
 import RPi.GPIO as GPIO
-import time
+# import time
 from flask import Flask
 from adafruit_motorkit import MotorKit
-import threading
-import sys
+import wiringpi
+# import sys
 
 kit = MotorKit()
 
-speed = 0.4
-reveres_speed = -0.4
+# speed = 0.4
+# reveres_speed = -0.4
+GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
+GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)   # Set pin 4 to be an output pin and set initial
 
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    GPIO.cleanup()
-    sys.exit(0)
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+wiringpi.pwmWrite(18, 150)
 
 
-def set_interval(func, sec):
-    def func_wrapper():
-        set_interval(func, sec)
-        func()
-
-    t = threading.Timer(sec, func_wrapper)
-    t.start()
-    return t
+# def signal_handler(sig, frame):
+#     print('You pressed Ctrl+C!')
+#     GPIO.cleanup()
+#     sys.exit(0)
+#
+#
+# def set_interval(func, sec):
+#     def func_wrapper():
+#         set_interval(func, sec)
+#         func()
+#
+#     t = threading.Timer(sec, func_wrapper)
+#     t.start()
+#     return t
 
 
 def drive_forward():
@@ -45,6 +57,24 @@ def turn_right():
 def stop_driving():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
+
+def set_light():
+    channel_is_on = GPIO.input(4)
+    print('channel_is_on is {}'.format(channel_is_on))
+    if channel_is_on:
+        GPIO.output(4, GPIO.LOW)
+    else:
+        GPIO.output(4, GPIO.HIGH)
+
+def set_cameraUp():
+    wiringpi.pwmWrite(18, 250)
+
+def set_cameraDown():
+    wiringpi.pwmWrite(18, 50)
+
+def set_cameraCenter():
+    wiringpi.pwmWrite(18, 150)
+
 
 app = Flask(__name__)
 
@@ -77,6 +107,26 @@ def right():
 def stop():
     stop_driving()
     return 'Hello stop'
+
+@app.route('/light')
+def light():
+    set_light()
+    return 'Hello light'
+
+@app.route('/cameraUp')
+def cameraUp():
+    set_cameraUp()
+    return 'Hello set_cameraUp'
+
+@app.route('/cameraDown')
+def cameraDown():
+    set_cameraDown()
+    return 'Hello set_cameraDown'
+
+@app.route('/cameraCenter')
+def cameraCenter():
+    set_cameraCenter()
+    return 'Hello set_cameraCenter'
 
 
 if __name__ == '__main__':
