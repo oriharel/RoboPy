667b27f848a37d4cd622cba92f0bdba2e1440402
Ori Harel <ori@taranis.ag>
2020-08-01 18:59:54 +0300

    Bobby is talking!

--- robo.py
+++ robo.py
@@ -1,25 +1,29 @@
-import RPi.GPIO as GPIO
-# import time
+# import RPi.GPIO as GPIO
+import gtts
+import pygame
 from flask import Flask
+from flask import request
 from adafruit_motorkit import MotorKit
-import wiringpi
+import os
+# import wiringpi
 # import sys
 
 kit = MotorKit()
 
+
 # speed = 0.4
 # reveres_speed = -0.4
-GPIO.setwarnings(False)    # Ignore warning for now
-GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
-GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)   # Set pin 4 to be an output pin and set initial
+# GPIO.setwarnings(False)    # Ignore warning for now
+# GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
+# GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)   # Set pin 4 to be an output pin and set initial
 
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+# wiringpi.wiringPiSetupGpio()
+# wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+# wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
 
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
-wiringpi.pwmWrite(18, 75)
+# wiringpi.pwmSetClock(192)
+# wiringpi.pwmSetRange(2000)
+# wiringpi.pwmWrite(18, 75)
 
 
 # def signal_handler(sig, frame):
@@ -58,25 +62,25 @@ def stop_driving():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
 
-def set_light():
-    channel_is_on = GPIO.input(4)
-    print('channel_is_on is {}'.format(channel_is_on))
-    if channel_is_on:
-        GPIO.output(4, GPIO.LOW)
-    else:
-        GPIO.output(4, GPIO.HIGH)
+# def set_light():
+#     channel_is_on = GPIO.input(4)
+#     print('channel_is_on is {}'.format(channel_is_on))
+#     if channel_is_on:
+#         GPIO.output(4, GPIO.LOW)
+#     else:
+#         GPIO.output(4, GPIO.HIGH)
 
-def set_camera(ms):
-    wiringpi.pwmWrite(18, ms)
-
-def set_cameraUp():
-    wiringpi.pwmWrite(18, 250)
-
-def set_cameraDown():
-    wiringpi.pwmWrite(18, 40)
-
-def set_cameraCenter():
-    wiringpi.pwmWrite(18, 150)
+# def set_camera(ms):
+#     wiringpi.pwmWrite(18, ms)
+#
+# def set_cameraUp():
+#     wiringpi.pwmWrite(18, 250)
+#
+# def set_cameraDown():
+#     wiringpi.pwmWrite(18, 40)
+#
+# def set_cameraCenter():
+#     wiringpi.pwmWrite(18, 150)
 
 
 app = Flask(__name__)
@@ -111,30 +115,44 @@ def stop():
     stop_driving()
     return 'Hello stop'
 
-@app.route('/light')
-def light():
-    set_light()
-    return 'Hello light'
+# @app.route('/light')
+# def light():
+#     set_light()
+#     return 'Hello light'
+#
+# @app.route('/cameraUp')
+# def cameraUp():
+#     set_cameraUp()
+#     return 'Hello set_cameraUp'
+#
+# @app.route('/cameraDown')
+# def cameraDown():
+#     set_cameraDown()
+#     return 'Hello set_cameraDown'
+#
+# @app.route('/cameraCenter')
+# def cameraCenter():
+#     set_cameraCenter()
+#     return 'Hello set_cameraCenter'\
 
-@app.route('/cameraUp')
-def cameraUp():
-    set_cameraUp()
-    return 'Hello set_cameraUp'
+# @app.route('/cameraSet/<int:ms>')
+# def cameraSet(ms):
+#     set_camera(ms)
+#     return 'Hello set_camera'
 
-@app.route('/cameraDown')
-def cameraDown():
-    set_cameraDown()
-    return 'Hello set_cameraDown'
-
-@app.route('/cameraCenter')
-def cameraCenter():
-    set_cameraCenter()
-    return 'Hello set_cameraCenter'\
-
-@app.route('/cameraSet/<int:ms>')
-def cameraSet(ms):
-    set_camera(ms)
-    return 'Hello set_camera'
+@app.route('/speak', methods=['POST'])
+def speak():
+    req_data = request.get_json()
+    text = req_data['text']
+    tts = gtts.gTTS(text)
+    os.remove("text.mp3")
+    tts.save("text.mp3")
+    pygame.mixer.init()
+    pygame.mixer.music.load("text.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+    return 'Hello speak'
 
 
 if __name__ == '__main__':
